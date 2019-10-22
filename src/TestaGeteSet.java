6c53975d39c5fa7ac8a5f58a29b1163acc5af7c5
Cristian Welter de Jesus <cristian.jesus@db1.com.br>
2019-10-22 17:24:05 -0300

    Classe cliente e conta com gets e sets

--- src/TestaGeteSet.java
+++ src/TestaGeteSet.java
@@ -5,12 +5,18 @@ public class TestaGeteSet {
         conta.deposita(100);
         System.out.println("Saldo: "+ conta.getSaldo());
 
-        conta.setTitular(new Cliente());
+        Cliente paulo = new Cliente();
+        paulo.setNome("Paulo");
+        paulo.setCpf("123.456.123-42");
+        paulo.setProfissao("programador");
 
         conta.setAgencia(386);
         System.out.println("Agencia: "+ conta.getAgencia());
 
         conta.setNumero(1337);
         System.out.println("Número: "+ conta.getNumero());
+
+        conta.setTitular(paulo);
+        System.out.println("Titular da conta numero "+conta.getNumero()+" : "+conta.getTitular().getNome());
     }
 }
--- src/TesteClienteConta.java
+++ src/TesteClienteConta.java
@@ -2,11 +2,11 @@ public class TesteClienteConta {
     public static void main(String[] args) {
         Conta contadoJoao= new Conta();
         Cliente joao = new Cliente();
-        joao.nome="João";
+        joao.setNome("João");
 
         contadoJoao.setTitular(joao);
 
         contadoJoao.deposita(500);
-        System.out.printf("Conta com o saldo de " + contadoJoao.getSaldo()+ " pertence ao " + contadoJoao.getTitular().nome);
+        System.out.printf("Conta com o saldo de " + contadoJoao.getSaldo()+ " pertence ao " + contadoJoao.getTitular().getNome());
     }
 }
